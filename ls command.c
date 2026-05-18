9e17cd80e3c708595f7b1b5447f1c2f759bca81c
NAVEENKUMAR T <naveenkumarsnr28@gmail.com>
2026-05-19 00:14:15 +0530

    Add files via upload

---
+++ ls command.c
@@ -0,0 +1,30 @@
+/* ls command simulation */
+
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+
+int main()
+{
+    struct dirent **namelist;
+    int n, i;
+    char pathname[100];
+
+    getcwd(pathname, sizeof(pathname));
+
+    n = scandir(pathname, &namelist, 0, alphasort);
+
+    if(n < 0)
+        printf("Error\n");
+
+    else
+    {
+        for(i=0; i<n; i++)
+        {
+            if(namelist[i]->d_name[0] != '.')
+                printf("%-20s", namelist[i]->d_name);
+        }
+    }
+
+    return 0;
+}
